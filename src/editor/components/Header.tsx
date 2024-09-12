523f34779d0a2a3b58ace4c1df3b93683c77f42f
Browinee <nihilitypeo@gmail.com>
2024-09-12 12:58:57 +0800

    feat: add preview

--- src/editor/components/Header.tsx
+++ src/editor/components/Header.tsx
@@ -1,3 +1,37 @@
+import { Button, Space } from "antd";
+import { useComponentsStore } from "../stores/components";
+
 export function Header() {
-  return <div>Header</div>;
+  const { mode, setMode, setCurComponentId } = useComponentsStore();
+
+  return (
+    <div className="w-[100%] h-[100%]">
+      <div className="h-[50px] flex justify-between items-center px-[20px]">
+        <div>LowCode</div>
+        <Space>
+          {mode === "edit" && (
+            <Button
+              onClick={() => {
+                setMode("preview");
+                setCurComponentId(null);
+              }}
+              type="primary"
+            >
+              Preview
+            </Button>
+          )}
+          {mode === "preview" && (
+            <Button
+              onClick={() => {
+                setMode("edit");
+              }}
+              type="primary"
+            >
+              Exit preview
+            </Button>
+          )}
+        </Space>
+      </div>
+    </div>
+  );
 }
